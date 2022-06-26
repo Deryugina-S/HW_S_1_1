f8c814ecedc0c5f9a96496dac114f288b94778d2
Svetlana <lankaby@yandex.ru>
2022-06-26 08:32:07 +0300

    Ответ на 1ю задачу

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: a ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: b ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+  Console.WriteLine("Max" + a);  
+}
+
+else 
+{
+    Console.WriteLine("Max = " + b);    
+}
